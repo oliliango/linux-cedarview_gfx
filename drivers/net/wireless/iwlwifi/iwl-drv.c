78825204a6f550e2d08ce5a9cb24be81a9662eb0
Daniel Leung <daniel.leung@intel.com>
2012-10-18 14:46:15 -0700

    MUST_REVERT: iwlwifi: disable plcp_check and 11n
    There are some issues with plcp_check and 802.11n support,
    so they need to be disabled by default.
    
    Change-Id: I6e97ac2a4e496fab0d4bb6b0c5e69f27f61e4f68
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwlwifi/iwl-drv.c
@@ -1095,12 +1095,13 @@ void iwl_drv_stop(struct iwl_drv *drv)
 struct iwl_mod_params iwlwifi_mod_params = {
 	.amsdu_size_8K = 1,
 	.restart_fw = 1,
-	.plcp_check = true,
+	.plcp_check = false,
 	.bt_coex_active = true,
 	.power_level = IWL_POWER_INDEX_1,
 	.bt_ch_announce = true,
 	.auto_agg = true,
 	.wd_disable = true,
+	.disable_11n = 1,
 	/* the rest are 0 by default */
 };
 EXPORT_SYMBOL_GPL(iwlwifi_mod_params);
